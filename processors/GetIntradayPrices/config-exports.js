ef79b41d20f6c9e53feb7ac9b1fa6d93b571d835
Tan <jantan@38f9d3e227ba.ant.amazon.com>
2020-06-30 18:25:19 +0100

    Changed template config to match example

--- processors/GetIntradayPrices/config-exports.js
+++ processors/GetIntradayPrices/config-exports.js
@@ -1,10 +1,10 @@
 const config = {
-    cognito_pool_id: "eu-west-1_iprbPnHwy",
-    cognito_backend_client_id: "10kjg7v2nm0tc03plaso6nkapa",
-    cognito_backend_access_key: "stockMonitoringDashboard/dev/backend",
-    appsync_endpoint_url: "https://xmzyer66yfdxjjkrphcyesqxqu.appsync-api.eu-west-1.amazonaws.com/graphql",
+    cognito_pool_id: "eu-west-1_DMhZ3rrXS",
+    cognito_backend_client_id: "pq3rcra4kend08leir0lbth06",
+    cognito_backend_access_key: "stockMonitoring/local/backend",
+    appsync_endpoint_url: "https://efrgcmtrnfcn7oshfxf2kvsx4y.appsync-api.eu-west-1.amazonaws.com/graphql",
     appsync_region: "eu-west-1",
-    data_feed_key: "stockMonitoringDashboard/dev/datafeed",
+    data_feed_key: "stockMonitoring/local/datafeed",
     feed_interval: "30min"
 }
 
